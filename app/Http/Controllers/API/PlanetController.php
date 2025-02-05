42036d2cfe9610733672baeab3b1a28a3418094c
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-05 18:28:28 +0100

    cambiato nuovamente namespace

--- app/Http/Controllers/API/PlanetController.php
+++ app/Http/Controllers/API/PlanetController.php
@@ -1,28 +1,21 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use App\Models\Planet;
-use Illuminate\Http\Request;
 
 class PlanetController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $data = Planet::all();
         return $data;
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $name)
     {
         $data = Planet::where('name', $name)->get();
         return $data;
     }
-
 }
