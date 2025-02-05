0b46c749fe1ff48eca5004f6cf020dac05528fff
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-05 17:06:59 +0100

    migration types table e aggiunta del type_id column alla tab principale Planets

---
+++ database/migrations/2025_02_05_154801_add_type_to_planets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('planets', function (Blueprint $table) {
+            $table->foreignId('type_id')->nullable()->constrained()->onUpdate('cascade')->onDelete(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('planets', function (Blueprint $table) {
+            Schema::withoutForeignKeyConstraints(function () {
+                Schema::dropIfExists('type_id');
+            });
+        });
+    }
+};
