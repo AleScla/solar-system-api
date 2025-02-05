2bbed2dfa94fd98b2ef6f27a83d7b63fa8c5508f
Alessandro Sclafani <scla.alessandro@gmail.com>
2025-02-05 18:28:50 +0100

    aggiunto prefix nel file appserviceprovider perchè altrimenti non venivano esposte le rotte

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\Route;
+
+
 
 use Illuminate\Support\ServiceProvider;
 
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Route::prefix('api')
+        ->middleware('api')
+        ->group(base_path('routes/api.php'));
     }
 }
